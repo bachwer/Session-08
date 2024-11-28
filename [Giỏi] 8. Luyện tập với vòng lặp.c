d700d6540e0fd9431849616402c23130370fd39d
bachwer <110678064+bachwer@users.noreply.github.com>
2024-11-28 23:37:34 +0700

    Create [Giỏi] 8. Luyện tập với vòng lặp.c

---
+++ [Giỏi] 8. Luyện tập với vòng lặp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    int num1[5][5] = {
+    {2, 3, 5, 7, 9},
+    {1, 3, 4, 8, 2},
+    {3, 4, 0, 1, 3},
+    {4, 5, 2, 1, 0},
+    {1, 2, 8, 9, 8}
+};
+int i;
+int x = 0;
+    for (int u = 0; u < 5; u++){
+        for(int k = 0; k < 5; k++){
+            if(k == 4 - x && u == 0 + x){
+                printf("%d ", num1[k][u]);
+                i += num1[k][u];
+                x++;
+            
+            }else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+ 
+    printf("total: %d", i);
+    return 0;
+}
