916b38492d1b2002443b0e2b0803c7bc7ed85e28
bachwer <110678064+bachwer@users.noreply.github.com>
2024-11-28 17:15:40 +0700

    Update [Khá] 6. Luyện tập với vòng lặp.c

--- [Khá] 6. Luyện tập với vòng lặp.c
+++ [Khá] 6. Luyện tập với vòng lặp.c
@@ -7,11 +7,12 @@ int main(){
     {4, 5, 2, 1, 0},
     {1, 2, 8, 9, 8}
 };
+int i;
     for (int u = 0; u < 5; u++){
         for(int k = 0; k < 5; k++){
             if(u == k ){
                 printf("%d ", num1[k][u]);
-              
+                i += num1[k][u];
             }else{
                 printf("  ");
             }
@@ -19,5 +20,6 @@ int main(){
         printf("\n");
     }
  
+    printf("total: %d", i);
     return 0;
 }
